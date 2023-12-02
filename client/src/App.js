6c9cf41cec30e98ae5b44d1d8d32a279775807d0
HansakaHeli <programminghhr66@gmail.com>
2023-12-02 19:21:51 +0530

    Create singup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
+import "./App.css"
 import { Button } from "@chakra-ui/react";
 import React from "react"
 import Router from "./Router";
 
 function App() {
   return (
-    <Router/>
+    <div className="App">
+      <Router/>
+    </div>
+    
   );
 }
 
--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,10 +1,43 @@
+import { Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text } from '@chakra-ui/react'
 import React from 'react'
+import Login from "../Components/Authentication/Login";
+import Signup from '../Components/Authentication/Signup';
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container maxW='xl' centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily={"Work sans"} color={"black"}  >Talk-A-Tive</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel><Login/></TabPanel>
+            <TabPanel><Signup/></TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 
